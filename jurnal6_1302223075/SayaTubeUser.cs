f2ae047dbb7ef7910e59ffc895ba0da0fa12a24b
unknown <darasheibaa@gmail.com>
2024-03-27 08:11:19 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ jurnal6_1302223075/SayaTubeUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnal6_1302223075
+{
+    internal class SayaTubeUser
+    {
+        private int id;
+        private List<SayaTubeVideo> uploadVideo;
+        private String username;
+
+        public SayaTubeUser(String username)
+        {
+            Random random = new Random();
+            this.id = random.Next(1111, 99999);
+            this.username = username;
+            this.uploadVideo = new List<SayaTubeVideo>();
+            
+        }
+        public int GetTotalVideoPlayCount()
+        {
+            int total = 0;
+            foreach (SayaTubeVideo video in uploadVideo)
+            {
+                total += video.GetPlayVideo();
+            }
+            return total;
+        }
+        public void AddVideo(SayaTubeVideo video)
+        {
+            this.uploadVideo.Add(video);
+        }
+        public void PrintAllVideoPlayCount()
+        {
+            Console.WriteLine("User: " + username);
+            int i = 0;
+            foreach (SayaTubeVideo video in uploadVideo)
+            {
+                Console.WriteLine("Video " + i);
+                video.PrintVideoDetails();
+                i++;
+            }
+            
+
+        }
+    }
+}
---
+++ jurnal6_1302223075/SayaTubeVideo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnal6_1302223075
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private String title;
+        private int playCount;
+
+        public SayaTubeVideo (String title)
+        {
+            Random random = new Random();
+            this.id = random.Next(1111, 99999);
+            this.title = title; 
+            this.playCount = playCount;
+            playCount = 0;
+        }
+        public void IncreasePlayCount(int playCount)
+        {
+            try
+            {
+                checked
+                {
+                    this.playCount = playCount;
+                }
+            }catch(OverflowException ex)
+            {
+                Console.WriteLine("Jumlah penambahan play count melebihi batas.");
+            }
+            
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID            : " + this.id);
+            Console.WriteLine("Judul         : " + this.title);
+            Console.WriteLine("Jumlah Tayang : " + this.playCount);
+        }
+        public int GetPlayVideo() { return playCount; }
+    }
+}
